336f21e4833c61c48753174074c0f862d3bc3858
alexredmond <alexredmond@zoho.com>
2017-01-18 16:04:14 +0000

    allow cors

--- SimpleRestApi/app.js
+++ SimpleRestApi/app.js
@@ -1,10 +1,15 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
+var cors = require('cors');
 var app = express();
  
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
  
 var routes = require("./routes/routes.js")(app);
  
